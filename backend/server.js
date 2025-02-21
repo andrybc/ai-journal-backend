7b7faad946d47023b7c1c921250f119e39ba0a46
andrybc <canelbandry@gmail.com>
2025-02-21 17:14:57 -0500

    basic setup with server.js for backend and vite create react app like the Professor wanted. Git ignoring .env and nodmodules etc

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,22 @@
 
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
-
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-
+// some middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// API Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes);
-
-
+// template route
 app.get('/', (req, res) => {
     res.send("Welcome to the Journal Organizer API");
 });
 
-// start the server on the configured port (default 3000)
+// start the server on port 3000 or from the .env file
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
