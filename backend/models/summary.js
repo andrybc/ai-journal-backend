177d77736720a99fe23f85a9e4d0183073830bef
WilliamQualls <williammqualls@gmail.com>
2025-02-26 16:25:29 -0500

    create summary schema

---
+++ backend/models/summary.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const autoIncrement = require('mongoose-auto-increment');
+
+// Define the summary schema
+const summarySchema = new mongoose.Schema({
+    summaryTitle: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    summaryContent: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    tagId: {
+        [Number]
+    },
+    userId: {
+        type: Number,
+        required: true
+    },
+    summaryId: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    timeCreated: {
+        type: Date,
+        default: Date.now
+    },
+    timeUpdated: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Summary', summarySchema);
