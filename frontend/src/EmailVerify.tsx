254731f73a939a23b0e503e90883ec45390c6ba3
Itz-Viggy <vigneshwar.sundararajan@gmail.com>
2025-03-26 11:10:06 -0400

    page changes

--- frontend/src/EmailVerify.tsx
+++ frontend/src/EmailVerify.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import Navbar from "./Navbar";
+import Navbar from "./components/Navbar";
 import { useState } from "react";
 
 const EmailVerify = () => {
