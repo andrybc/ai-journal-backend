2f59aa2c5e9fccdb1e9e6f3ac2fc1c6f9fa8f863
Itz-Viggy <vigneshwar.sundararajan@gmail.com>
2025-03-26 11:01:03 -0400

    Fixed navbar not reachable

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Navbar from "./Navbar";
+import Navbar from "../components/Navbar";
 import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
