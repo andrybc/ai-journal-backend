da5dc338567a91bf87d385f9f4d12c5fec99edb3
dhablichcastillo08 <da778455@ucf.edu>
2025-03-26 17:29:01 -0400

    adjustments made

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Relationships from "./pages/Relationships";
-import Notes from "./pages/Notes"
+import Notes from "./pages/Notes";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 
