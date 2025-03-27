acc9df6c0bca8f3d1e9010a06193145278c07be5
WilliamQualls <williammqualls@gmail.com>
2025-03-27 17:12:10 -0400

    added sendEmail.js

---
+++ backend/services/sendEmail.js
@@ -0,0 +1,20 @@
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const sendEmail = async(to, subject, content) => {
+    try {
+        const msg = {
+            to,
+            from: 'noreply@em919.journal-organizer.com',
+            subject,
+            html: content,
+        };
+        await sgMail.send(msg);
+        console.log(`Email sent to ${to}`);
+    } catch (error) {
+        console.error('Error sending email:', error);
+        throw error;
+    }
+};
+
+module.exports = sendEmail;
