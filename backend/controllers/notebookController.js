b9b08bfa7345b8f7ffcde0e06fa7c0aed16827ad
JoeBai3r <jo461915@ucf.edu>
2025-03-31 17:30:38 -0400

    resolved error handling

--- backend/controllers/notebookController.js
+++ backend/controllers/notebookController.js
@@ -118,6 +118,10 @@ exports.searchNotebooks = async (req, res) => {
 exports.getNotebookById = async (req, res) => {
   try {
     const { userId, notebookId } = req.params;
+	
+    if (!mongoose.Types.ObjectId.isValid(notebookId)) {
+      return res.status(400).json({ error: "Invalid notebookId" });
+    }
 
     const notebook = await Notebook.findOne({
       _id: notebookId,
